370a3235773669f04b0b86f8476851a41b98c2dc
高歌 <gaog@yzhj.com>
2020-12-10 15:37:39 +0800

    m

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,8 +2,8 @@
 module.exports = {
   NODE_ENV: '"production"',
   BASE_URI: '"http://223.80.107.190:38146/"',                          // 测试
-  BACK_WS_API: '"ws://192.168.1.176:5002/websocket/dcp/web/"',
+  BACK_WS_API: '"ws://223.80.107.190:5002/websocket/dcp/web/"',
   PLUGIN_WS_API: '"ws://localhost:17002"',
   MOCK: false,
-  VERSION: 202012101034,
+  VERSION: 202012101537,
 }
